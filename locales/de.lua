d76e7a6a9e62b18f9c832c2df62645358d89834a
GrafZahlGER <42323298+GrafZahlGER@users.noreply.github.com>
2021-09-29 14:58:25 +0200

    Uploaded

---
+++ locales/de.lua
@@ -0,0 +1,9 @@
+Locales['de'] = {
+	['shopname'] = 'Geschäft',
+	['press_menu'] = '[~y~E~s~] auf Geschäft zugreifen',
+	['bought'] = 'Du hast %sx %s für $%s gekauft',
+	['not_enough'] = 'Du hast nicht genug Geld: %s',
+	['player_cannot_hold'] = 'Du trägst zu viel mit dir rum',
+	['no'] = 'Nein',
+	['yes'] = 'Ja',
+}
